fc6cb5bb0875d152ffccea9b7ade303fdc78ccc1
Yasin Baltaoğlu <37988334+ayasinb@users.noreply.github.com>
2024-05-02 22:40:10 +0300

    Add project files.

---
+++ Travela.WebApi/Controllers/DestinationController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Travela.BusinessLayer.Abstract;
+using Travela.EntityLayer.Concrete;
+
+namespace Travela.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DestinationController : ControllerBase
+    {
+        private readonly IDestinationService _destinationService;
+
+        public DestinationController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        [HttpGet]
+        public IActionResult DestinationList()
+        {
+            var values= _destinationService.TGetListAll();
+            return Ok(values);
+        }
+
+        [HttpPost]  
+        public IActionResult CreateDestination(Destination destination)
+        {
+            _destinationService.TInsert(destination);
+            return Ok("Rota Başarıyla Oluşturuldu!");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteDestination(int id)
+        { 
+           _destinationService.TDelete(id);
+            return Ok("Rota Silme İşlemi Başarılı!");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateDestination(Destination destination) 
+        {
+            _destinationService.TUpdate(destination);
+            return Ok("Güncelleme İşlemi Başarılı!");
+        }
+
+        [HttpGet("GetDestination")]
+        public IActionResult GetDestination(int id) 
+        { 
+            return Ok(_destinationService.TGetById(id));
+        }
+    }
+}
---
+++ Travela.WebUI/Dtos/ResultCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Travela.WebUI.Dtos
+{
+    public class ResultCategoryDto
+    {      
+            public int categoryId { get; set; }
+            public string categoryName { get; set; }
+            public string imageUrl { get; set; }
+     }
+
+    
+}
